f8bf5e916de33916b4bde37c557791544873de3f
Harrison <harrison@theshoebridges.com>
2015-07-28 14:53:52 +1000

    Add basic angular routing and rendering

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,71 @@
-var app = angular.module('steel', []);
+var app = angular.module('steel', ['ngRoute']);
 
-app.controller('homeCtrl', ['$scope', function($scope){
-    $scope.message = 'Hello!';
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+        when('/assignments', {
+            templateUrl: 'templates/assignments.html'
+        }).
+        when('/', {
+            templateUrl: 'templates/feed.html',
+            controller: 'FeedCtrl'
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+}]);
+
+app.controller('FeedCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.dueTasks = [
+        {
+            "name": "Funny Strings",
+            "id": 1,
+            "done": false
+        },
+        {
+            "name": "Reverse String",
+            "id": 2,
+            "done": false
+        },
+        {
+            "name": "Utopian Tree",
+            "id": 3,
+            "done": true
+        },
+        {
+            "name": "Print",
+            "id": 4,
+            "done": true
+        }
+    ];
+
+    $scope.feedUpdates = [
+        {
+            "type": "assignment",
+            "from": "Greg",
+            "time": "20 minutes ago",
+            "message": "Greg posted a new assignment"
+        },
+        {
+            "type": "feedback",
+            "from": "Greg",
+            "time": "5 hours ago",
+            "message": "Greg gave feedback on your \"Funny Strings\" task"
+        },
+        {
+            "type": "like",
+            "from": "Jimmy",
+            "time": "10 hours ago",
+            "message": "Jimmy liked your post"
+        },
+        {
+            "type": "request",
+            "from": "James",
+            "time": "1 day ago",
+            "message": "James requested to work with you on the Doomsday assignment"
+        }
+    ];
+    
+    $scope.allTasks = [
+        
+    ];
 }]);
