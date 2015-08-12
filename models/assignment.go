292a1388874aa91c20c2ed320aa385411b198c85
Harrison <harrison@theshoebridges.com>
2015-08-12 13:02:28 +1000

    Move Submission work into submission.go

--- models/assignment.go
+++ models/assignment.go
@@ -6,10 +6,6 @@ import (
 	"time"
 )
 
-const (
-	timeFormat = "2006-01-02 15:04:05.999999999 -0700 MST"
-)
-
 type Assignment struct {
 	ID          int64
 	Name        string
@@ -23,19 +19,6 @@ func (a *Assignment) Delete() error {
 
 	return err
 }
-
-type Submission struct {
-	ID       int64
-	Thoughts string
-	TeamName string
-}
-
-type SubmissionMember struct {
-	ID           int64
-	SubmissionID int64
-	UserID       int64
-}
-
 func GetAssignment(id int64) (Assignment, error) {
 	a := Assignment{
 		ID: id,
@@ -55,53 +38,4 @@ func GetAssignment(id int64) (Assignment, error) {
 	}
 
 	return a, nil
-}
-
-func GetSubmission(id int64) (Submission, error) {
-	s := Submission{
-		ID: id,
-	}
-
-	row := db.QueryRow("SELECT team_name, thoughts FROM submissions WHERE id = ?", s.ID)
-	err := row.Scan(&s.TeamName, &s.Thoughts)
-
-	return s, err
-}
-
-func (s *Submission) AddMember(id int64) error {
-	row := db.QueryRow("SELECT user FROM team_members WHERE submission = ? AND user = ?", s.ID, id)
-
-	var uid *int64
-	err := row.Scan(uid)
-
-	if uid != nil || (err != nil && err != sql.ErrNoRows) {
-		return errors.New("This user is alreayd a member")
-	}
-
-	_, err = db.Exec("INSERT INTO team_members (submission, user) VALUES (?, ?)", s.ID, id)
-
-	return err
-}
-
-func (s *Submission) Members() ([]User, error) {
-	var us []User
-	rows, err := db.Query("SELECT user FROM team_members WHERE submission = ?", s.ID)
-	if err != nil {
-		return us, err
-	}
-
-	defer rows.Close()
-
-	for rows.Next() {
-		var uid int64
-		rows.Scan(&uid)
-		u, err := GetUserByID("id", uid)
-		if err != nil {
-			return us, err
-		}
-
-		us = append(us, u)
-	}
-
-	return us, nil
 }
---
+++ models/submission.go
@@ -0,0 +1,62 @@
+package models
+
+type Submission struct {
+	ID       int64
+	Thoughts string
+	TeamName string
+}
+
+type SubmissionMember struct {
+	ID           int64
+	SubmissionID int64
+	UserID       int64
+}
+
+func GetSubmission(id int64) (Submission, error) {
+	s := Submission{
+		ID: id,
+	}
+
+	row := db.QueryRow("SELECT team_name, thoughts FROM submissions WHERE id = ?", s.ID)
+	err := row.Scan(&s.TeamName, &s.Thoughts)
+
+	return s, err
+}
+
+func (s *Submission) AddMember(id int64) error {
+	row := db.QueryRow("SELECT user FROM team_members WHERE submission = ? AND user = ?", s.ID, id)
+
+	var uid *int64
+	err := row.Scan(uid)
+
+	if uid != nil || (err != nil && err != sql.ErrNoRows) {
+		return errors.New("This user is alreayd a member")
+	}
+
+	_, err = db.Exec("INSERT INTO team_members (submission, user) VALUES (?, ?)", s.ID, id)
+
+	return err
+}
+
+func (s *Submission) Members() ([]User, error) {
+	var us []User
+	rows, err := db.Query("SELECT user FROM team_members WHERE submission = ?", s.ID)
+	if err != nil {
+		return us, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var uid int64
+		rows.Scan(&uid)
+		u, err := GetUserByID("id", uid)
+		if err != nil {
+			return us, err
+		}
+
+		us = append(us, u)
+	}
+
+	return us, nil
+}
