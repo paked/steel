ccee822e9d849ee4daeb0bd74936172fcfa2f362
Harrison <harrison@theshoebridges.com>
2015-08-17 16:50:03 +1000

    Add students addition to class

--- models/class.go
+++ models/class.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"database/sql"
+	"errors"
+)
+
 type Class struct {
 	ID          int64
 	Name        string
@@ -24,4 +29,16 @@ func NewClass(name, description string) (Class, error) {
 	}
 
 	return c, nil
+}
+
+func (c *Class) AddUser(u User) error {
+	rows := db.QueryRow("SELECT id FROM students WHERE user = ? AND class = ?", u.ID, c.ID)
+	err := rows.Scan() // why does this work...
+	if err != sql.ErrNoRows {
+		return errors.New("That user is already in this class")
+	}
+
+	_, err = db.Exec("INSERT INTO students (user, class) VALUES (?, ?)", u.ID, c.ID)
+
+	return err
 }
--- models/class_test.go
+++ models/class_test.go
@@ -5,6 +5,7 @@ import (
 )
 
 func TestClass(t *testing.T) {
+	u, err := RegisterUser("class_tester", "pw", "matt")
 	c, err := NewClass("My Class", "A class for learning JavaScript")
 	if err != nil {
 		t.Error("Could not create class:", err)
@@ -12,5 +13,15 @@ func TestClass(t *testing.T) {
 
 	if c.Name != "My Class" || c.Description != "A class for learning JavaScript" {
 		t.Error("Incorrect information in class")
+	}
+
+	err = c.AddUser(u)
+	if err != nil {
+		t.Error("Could not add user")
+	}
+
+	err = c.AddUser(u)
+	if err == nil {
+		t.Error("Was able to add the same student twice to a class")
 	}
 }
